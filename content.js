af4470e2e418c51ad10f26d31afe983659ff5e72
Tobes <tobes@longtaildragon.com>
2023-05-22 23:41:30 -0600

    MakeDreamweaverAwesome

--- content.js
+++ content.js
@@ -21,27 +21,38 @@
  
 */
 
-const targetNode = document.body;
+$(document).ready(function() {
+	
+	var title = document.title;
+	if (title == "Dreamweaver v3.0 - Google Docs"){
+		makeDreamweaverAwesome();
+	} else {
+		const targetNode = document.body;
 
-// Options for the observer (which mutations to observe)
-// Set attributes to false if you do not care if existing nodes have changed,
-//  otherwise set it true. 
-const config = { attributes: true, childList: true, subtree: true };
+		// Options for the observer (which mutations to observe)
+		// Set attributes to false if you do not care if existing nodes have changed,
+		//  otherwise set it true. 
+		const config = { attributes: true, childList: true, subtree: true };
 
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    makeItAwesomer();
-};
+		// Callback function to execute when mutations are observed
+		const callback = function(mutationsList, observer) {
+			makeAppsScriptEditorAwesome();
+		};
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+		// Create an observer instance linked to the callback function
+		const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+		// Start observing the target node for configured mutations
+		observer.observe(targetNode, config);
+	}
+	
+});
 
-
-
-function makeItAwesomer(){
+function makeAppsScriptEditorAwesome(){
+	
+	// .---------------------------.
+	// | Google Apps Script Editor |
+	// `---------------------------`
     
     var el = document.querySelector('#docs-chrome');
         if (el!==null) el.style.backgroundColor = "black";
@@ -57,8 +68,16 @@ function makeItAwesomer(){
         if (el!==null) el.style.backgroundColor = "black";
     el = document.querySelector('.docs-horizontal-ruler');
         if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "black";
     el = document.querySelector('.docs-ruler-face');
         if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "black";
+	el = document.querySelector('#kix-horizontal-ruler-container');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "black";
+	el = document.querySelector('#kix-horizontal-ruler');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "black";
 
     el = document.querySelector('.waffle-name-box-container');
         if (el!==null) el.style.backgroundColor = "black";
@@ -236,5 +255,159 @@ function makeItAwesomer(){
 		/* Apps Script Services */
         el.style.color = "darkGreen";
     });
+	
+}
+
+function makeDreamweaverAwesome(){
+	
+	// .--------------------.
+	// | Google Docs Editor |
+    // `--------------------`
+	
+	var el = document.querySelector('#docs-chrome');
+		if (el!==null) el.style.backgroundColor = "black";
+	var el = document.querySelector('.docs-material');
+		if (el!==null) el.style.backgroundColor = "black";
+	var el = document.querySelector('#docs-header');
+		if (el!==null) el.style.backgroundColor = "black";
+	var el = document.querySelector('.docs-titlebar-buttons');
+		if (el!==null) el.style.backgroundColor = "black";
+	var el = document.querySelector('#docs-toolbar-mode-switcher');
+		if (el!==null) el.style.backgroundColor = "black";
+    var el = document.querySelector('#docs-toolbar-wrapper');
+		if (el!==null) el.style.backgroundColor = "black";
+    var el = document.querySelector('.docs-horizontal-ruler');
+		if (el!==null) el.style.backgroundColor = "black";
+    var el = document.querySelector('.docs-ruler-face');
+		if (el!==null) el.style.backgroundColor = "black";
+	el = document.querySelector('#kix-horizontal-ruler-container');
+		if (el!==null) el.style.backgroundColor = "black !important";
+		if (el!==null) el.style.color = "black !important";
+	el = document.querySelector('#kix-horizontal-ruler');
+		if (el!==null) el.style.backgroundColor = "black !important";
+		if (el!==null) el.style.color = "black !important";
+	var el = document.querySelector('#kix-horizontal-ruler.docs-grille-gm3.docs-body-pageless .docs-horizontal-ruler, .docs-grille-gm3.docs-body-pageless .docs-ruler-background-inner, .docs-grille-gm3.docs-body-pageless .docs-vertical-ruler');
+		if (el!==null) el.style.backgroundColor = "black !important";
+	
+	var el = document.querySelector('.waffle-name-box-container');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "green";
+	
+	var el = document.querySelector('#t-name-box');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "green";
+	
+	var el = document.querySelector('#formula-bar');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "green";
+	
+	var el = document.querySelector('.cell-input');
+		if (el!==null) el.style.backgroundColor = "black";
+		if (el!==null) el.style.color = "green";
+		
+	var el = document.querySelector('.fal ');
+		if (el!==null) el.style.color = "green";
+    
+    var el = document.querySelector('.docs-title-input');
+		if (el!==null) el.style.backgroundColor = "green";
+    var el = document.querySelector('.docs-title-input');
+		if (el!==null) el.style.border = "none";
+    var el = document.querySelector('.docs-title-input');
+		if (el!==null) el.style.color = "black";
+    var el = document.querySelector('.docs-title-input');
+		if (el!==null) el.style.fontFamily = "'Source Code Pro', monospace;";
+    
+	var el = document.querySelector('#docs-meet-in-editors-entrypointbutton');
+		if (el!==null) el.style.background = "black";
+	
+    var el = document.querySelector('.docs-ui-unprintable');
+		if (el!==null) el.style.background = "green";
+    var el = document.querySelector('.left-sidebar-container.pageless-format');
+		if (el!==null) el.style.background = "darkGreen";
+    var el = document.querySelector('.left-sidebar-container.pageless-format');
+		if (el!==null) el.style.boxShadow = "1px 0 0 0 darkGreen";
+	
+    var el = document.querySelector('.jfk-button');
+		if (el!==null) el.style.background = "green";
+	var el = document.querySelector('.jfk-button-action');
+		if (el!==null) el.style.background = "green";
+	var el = document.querySelector('.docs-titlebar-button');
+		if (el!==null) el.style.background = "green";
+	var el = document.querySelector('.navigation-widget-navigation-tab-button.navigation-widget');
+		if (el!==null) el.style.background = "black";
+    
+    var el = document.querySelector('.docs-gm #docs-toolbar-wrapper');
+		if (el!==null) el.style.borderTop = "1px solid green";
+    var el = document.querySelector('.docs-gm #docs-toolbar-wrapper');
+		if (el!==null) el.style.borderBottom = "1px solid green";
+    var el = document.querySelector('.docs-horizontal-ruler');
+		if (el!==null) el.style.borderBottom = "1px solid green";
+    
+    var el = document.querySelector('.navigation-widget-header');
+		if (el!==null) el.style.color = "green";
+		
+	var el = document.querySelector('#gb');
+		if (el!==null) el.style.backgroundColor = "black";
+		
+	var el = document.querySelector('.KOiXb');
+		if (el!==null) el.style.color = "green";
+		
+	var el = document.querySelector('.Gn5yxe');
+		if (el!==null) el.style.backgroundColor = "green";
+		
+	var el = document.querySelector('.ZHQ5U.ZHQ5U');
+		if (el!==null) el.style.backgroundColor = "black";
+		
+	var el = document.querySelector('.td5WLe');
+		if (el!==null) el.style.backgroundColor = "black";
+		
+	var el = document.querySelector('.asc_FolderRoot');
+		if (el!==null) el.style.background = "black";
+		if (el!==null) el.style.color = "green";
+		
+	var el = document.querySelector('.UeVsd');
+		if (el!==null) el.style.color = "black";
+		if (el!==null) el.style.backgroundColor = "green";
+		
+	var el = document.querySelector('.monaco-editor.no-user-select .lines-content, .monaco-editor.no-user-select .view-line, .monaco-editor.no-user-select .view-lines');
+		if (el!==null) el.style.backgroundColor = "black";
+		
+	var el = document.querySelector('.monaco-editor .margin');
+		if (el!==null) el.style.backgroundColor = "black";		
+    
+    document.querySelectorAll('.goog-toolbar-button').forEach(function(el){
+        el.style.backgroundColor = "black";
+    });
+    
+    document.querySelectorAll('.goog-toolbar-menu-button').forEach(function(el){
+        el.style.backgroundColor = "black";
+    });
+    
+    document.querySelectorAll('.docs-toolbar-zoom-combobox').forEach(function(el){
+        el.style.backgroundColor = "green";
+    });
+    
+    document.querySelectorAll('.goog-toolbar-combo-button').forEach(function(el){
+        el.style.backgroundColor = "green";
+    });
+    
+    document.querySelectorAll('.goog-toolbar-select').forEach(function(el){
+        el.style.backgroundColor = "green";
+    });
+    
+    document.querySelectorAll('.menu-button').forEach(function(el){
+        el.style.color = "green";
+    });
+    
+    document.querySelectorAll('.goog-toolbar-separator').forEach(function(el){
+        el.style.color = "green";
+    });
+    
+    document.querySelectorAll('.navigation-item-content').forEach(function(el){
+        el.style.color = "green";
+        el.style.fontFamily = "'Source Code Pro', monospace;";
+    });
+	
+	setTimeout(makeDreamweaverAwesome,1);
     
 }
